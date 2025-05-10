6dbf282c046760f38e9ac53f3aa4700aa65592e7
R14552 <kubabo005@gmail.com>
2025-05-10 14:35:20 +0200

    account tests

---
+++ tests/HotelManagementApp.UnitTests/HandlerTests/AccountTests/ConfirmEmailCommandHandlerTests.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HotelManagementApp.Application.CQRS.Account.ConfirmEmail;
+using HotelManagementApp.Core.Dtos;
+using HotelManagementApp.Core.Interfaces.Identity;
+using HotelManagementApp.Core.Interfaces.Repositories.TokenRepositories;
+using HotelManagementApp.Core.Interfaces.Services;
+using HotelManagementApp.Core.Models.TokenModels;
+using Moq;
+using Xunit;
+
+public class ConfirmEmailCommandHandlerTests
+{
+    private readonly Mock<IUserManager> _userManagerMock;
+    private readonly Mock<IConfirmEmailTokensRepository> _tokenRepositoryMock;
+    private readonly Mock<ITokenService> _tokenServiceMock;
+    private readonly ConfirmEmailCommandHandler _handler;
+
+    public ConfirmEmailCommandHandlerTests()
+    {
+        _userManagerMock = new Mock<IUserManager>();
+        _tokenRepositoryMock = new Mock<IConfirmEmailTokensRepository>();
+        _tokenServiceMock = new Mock<ITokenService>();
+        _handler = new ConfirmEmailCommandHandler(_userManagerMock.Object, _tokenRepositoryMock.Object, _tokenServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldConfirmEmail_WhenTokenIsValid()
+    {
+        var user = new UserDto
+        {
+            Id = "123",
+            Email = "test@gmail.com",
+            UserName = "testuser",
+            Roles = new List<string>
+            {
+                "Client"
+            },
+            IsEmailConfirmed = false
+        };
+        var token = new ConfirmEmailToken { UserId = "123", ExpirationDate = DateTime.UtcNow.AddMinutes(10), ConfirmEmailTokenHash = "example" };
+        _userManagerMock.Setup(x => x.FindByIdAsync("123")).ReturnsAsync(user);
+        _tokenServiceMock.Setup(x => x.GetTokenHash(It.IsAny<string>())).Returns("hashedToken");
+        _tokenRepositoryMock.Setup(x => x.GetToken("hashedToken", It.IsAny<CancellationToken>())).ReturnsAsync(token);
+
+        await _handler.Handle(new ConfirmEmailCommand { UserId = "123", Token = "token" }, CancellationToken.None);
+
+        Assert.True(user.IsEmailConfirmed);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowUnauthorizedAccessException_WhenUserNotFound()
+    {
+        _userManagerMock.Setup(x => x.FindByIdAsync("123")).ReturnsAsync((UserDto?)null);
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() =>
+            _handler.Handle(new ConfirmEmailCommand { UserId = "123", Token = "token" }, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowUnauthorizedAccessException_WhenTokenIsInvalid()
+    {
+        var user = new UserDto
+        {
+            Id = "123",
+            Email = "test@gmail.com",
+            UserName = "testuser",
+            Roles = new List<string>
+            {
+                "Client"
+            },
+            IsEmailConfirmed = false
+        };
+        _userManagerMock.Setup(x => x.FindByIdAsync("123")).ReturnsAsync(user);
+        _tokenServiceMock.Setup(x => x.GetTokenHash(It.IsAny<string>())).Returns((string?)null);
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() =>
+            _handler.Handle(new ConfirmEmailCommand { UserId = "123", Token = "token" }, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowUnauthorizedAccessException_WhenTokenIsExpired()
+    {
+        var user = new UserDto
+        {
+            Id = "123",
+            Email = "test@gmail.com",
+            UserName = "testuser",
+            Roles = new List<string>
+            {
+                "Client"
+            },
+            IsEmailConfirmed = false
+        };
+        var token = new ConfirmEmailToken { UserId = "123", ExpirationDate = DateTime.UtcNow.AddMinutes(-10), ConfirmEmailTokenHash = "example" };
+        _userManagerMock.Setup(x => x.FindByIdAsync("123")).ReturnsAsync(user);
+        _tokenServiceMock.Setup(x => x.GetTokenHash(It.IsAny<string>())).Returns("hashedToken");
+        _tokenRepositoryMock.Setup(x => x.GetToken("hashedToken", It.IsAny<CancellationToken>())).ReturnsAsync(token);
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() =>
+            _handler.Handle(new ConfirmEmailCommand { UserId = "123", Token = "token" }, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowInvalidOperationException_WhenEmailAlreadyConfirmed()
+    {
+        var user = new UserDto
+        {
+            Id = "123",
+            Email = "test@gmail.com",
+            UserName = "testuser",
+            Roles = new List<string>
+            {
+                "Client"
+            },
+            IsEmailConfirmed = true
+        };
+        var token = new ConfirmEmailToken { UserId = "123", ExpirationDate = DateTime.UtcNow.AddMinutes(10), ConfirmEmailTokenHash = "example" };
+        _userManagerMock.Setup(x => x.FindByIdAsync("123")).ReturnsAsync(user);
+        _tokenServiceMock.Setup(x => x.GetTokenHash(It.IsAny<string>())).Returns("hashedToken");
+        _tokenRepositoryMock.Setup(x => x.GetToken("hashedToken", It.IsAny<CancellationToken>())).ReturnsAsync(token);
+
+        await Assert.ThrowsAsync<InvalidOperationException>(() =>
+            _handler.Handle(new ConfirmEmailCommand { UserId = "123", Token = "token" }, CancellationToken.None));
+    }
+}
---
+++ tests/HotelManagementApp.UnitTests/HandlerTests/AccountTests/GetAccountsWithoutRoleQueryHandlerTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using HotelManagementApp.Application.CQRS.Account.GetAccountsWithoutRole;
+using HotelManagementApp.Application.Responses.AccountResponses;
+using HotelManagementApp.Core.Dtos;
+using HotelManagementApp.Core.Exceptions.NotFound;
+using HotelManagementApp.Core.Interfaces.Identity;
+using HotelManagementApp.Core.Interfaces.Repositories.AccountRepositories;
+using HotelManagementApp.Core.Interfaces.Services;
+using HotelManagementApp.Core.Models;
+using HotelManagementApp.Core.Models.AccountModels;
+using Moq;
+using Xunit;
+
+public class GetAccountsWithoutRoleQueryHandlerTests
+{
+    private readonly Mock<IUserManager> _userManagerMock;
+    private readonly Mock<IFileService> _fileServiceMock;
+    private readonly Mock<IProfilePictureRepository> _profilePictureRepositoryMock;
+    private readonly GetAccountsWithoutRoleQueryHandler _handler;
+
+    public GetAccountsWithoutRoleQueryHandlerTests()
+    {
+        _userManagerMock = new Mock<IUserManager>();
+        _fileServiceMock = new Mock<IFileService>();
+        _profilePictureRepositoryMock = new Mock<IProfilePictureRepository>();
+        _handler = new GetAccountsWithoutRoleQueryHandler(_userManagerMock.Object, _fileServiceMock.Object, _profilePictureRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnAccountsWithoutRole()
+    {
+        var users = new List<UserDto>
+        {
+            new UserDto { Id = "123", UserName = "User1", Email = "user1@example.com", Roles = new List<string>() }
+        };
+        var profilePicture = new ProfilePicture { UserId = "123", FileName = "profile1.jpg" };
+        _userManagerMock.Setup(x => x.GetUsersWithoutRole()).ReturnsAsync(users);
+        _profilePictureRepositoryMock.Setup(x => x.GetProfilePicture("123", It.IsAny<CancellationToken>())).ReturnsAsync(profilePicture);
+        _fileServiceMock.Setup(x => x.GetFileUrl("images", "profile1.jpg")).Returns("http://example.com/images/profile1.jpg");
+
+        var result = await _handler.Handle(new GetAccountsWithoutRoleQuery(), CancellationToken.None);
+
+        Assert.Single(result);
+        Assert.Equal("User1", result.First().UserName);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowProfilePictureNotFoundException_WhenProfilePictureIsMissing()
+    {
+        var users = new List<UserDto>
+        {
+            new UserDto { Id = "123", UserName = "User1", Email = "user1@example.com", Roles = new List<string>() }
+        };
+        _userManagerMock.Setup(x => x.GetUsersWithoutRole()).ReturnsAsync(users);
+        _profilePictureRepositoryMock.Setup(x => x.GetProfilePicture("123", It.IsAny<CancellationToken>())).ReturnsAsync((ProfilePicture?)null);
+
+        await Assert.ThrowsAsync<ProfilePictureNotFoundException>(() =>
+            _handler.Handle(new GetAccountsWithoutRoleQuery(), CancellationToken.None));
+    }
+}
